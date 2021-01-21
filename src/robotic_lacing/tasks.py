b28fa53c2fd6f76158a188a3453ee16e2a23f10c
laurend <lauren@lauren.design>
2021-01-21 13:01:12 -0500

    Clean up initial test

--- src/robotic_lacing/tasks.py
+++ src/robotic_lacing/tasks.py
@@ -1,11 +1,2 @@
 def hi():
     print("hi!!!!!")
-
-class Octopus:
-    def __init__(self, name, color):
-        self.color = color
-        self.name = name
-
-    def tell_me_about_the_octopus(self):
-        print("This octopus is " + self.color + ".")
-        print(self.name + " is the octopus's name.")
