0855b3d5e30e1ab86d7aece6a43e327ecabc6058
laurend <lauren@lauren.design>
2021-01-21 12:56:42 -0500

    Create installable robotic_lacing python library

--- README.md
+++ README.md
@@ -3,13 +3,18 @@
 We make lace with robots
 
 ## Installation
-- Rhino 6
-- Compas: 0.15.6
-- CompasFab: 0.11.0
-- Docker
-- ABB communication (via CREATE Lab github. Integration with compas_fab objects is WIP)
+- Rhino 6 or later
+- Compas: 1.0.0
+- CompasFab: 0.16.0
+- Robots plugin for grasshopper (temporary)
+- CREATE Lab ABB communication library
 
-## Visualization
+1. Clone this repo
+2. `cd` to the repo locally
+3. `pip install -e .` (don't forget the dot at the end)
+4. `python -m compas_rhino.install -p robotic_lacing`
+
+## Not needed now, but later for visualization
+- Docker (requires Windows Pro)
 `cd {{ path to repository }}\docker\abb_120`
-
 `docker-compose up -d`
---
+++ src/robotic_lacing/__init__.py
@@ -0,0 +1,36 @@
+"""
+********************************************************************************
+Robotic lacing
+********************************************************************************
+
+.. currentmodule:: robotic_lacing
+
+
+.. toctree::
+    :maxdepth: 1
+
+
+"""
+
+from __future__ import print_function
+
+import os
+import sys
+
+
+__author__ = ["Lauren Dreier"]
+__copyright__ = "2021"
+__license__ = "MIT License"
+__email__ = "ldreier@princeton.edu"
+__version__ = "0.1.0"
+
+
+HERE = os.path.dirname(__file__)
+
+HOME = os.path.abspath(os.path.join(HERE, "../../"))
+DATA = os.path.abspath(os.path.join(HOME, "data"))
+DOCS = os.path.abspath(os.path.join(HOME, "docs"))
+TEMP = os.path.abspath(os.path.join(HOME, "temp"))
+
+
+__all__ = ["HOME", "DATA", "DOCS", "TEMP"]
